820d6198755f8b97e0d74a8a5fa05c0425200b85
Joe Hand <joe@hand.email>
2017-07-31 19:06:48 -0700

    update

--- client/map.js
+++ client/map.js
@@ -10,23 +10,28 @@ var key = '5c9100073ba750b66b124dcd1854c039056a6b8cea34d115aa21c0750154d46d'
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9lYWhhbmQiLCJhIjoiaDd1MEJZQSJ9.fl3WTCt8MGNOSCGR_qqz7A'
 
 var initialFetchDone = false
+var fetchingData = false
 var mapLoaded = false
 var archive = hyperdrive(ram, key, {sparse: true})
 var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/joeahand/cj5rh94o22rqz2rqi7i2i5tkq',
-    center: [-122.6782433, 45.5252814],
-    zoom: 12
+  container: 'map', // container id
+  style: 'mapbox://styles/joeahand/cj5rh94o22rqz2rqi7i2i5tkq',
+  center: [-122.6782433, 45.5252814],
+  zoom: 12,
+  maxBounds: [
+      // Portland area bounds
+      [-123.1779407, 45.219892], // Southwest coordinates
+      [-122.2815557, 45.699901]  // Northeast coordinates
+  ]
 })
 var geojson = {
   type: 'FeatureCollection',
   features: []
 }
-var pendingData = []
 
 replicate()
 
-map.on('load', function() {
+map.on('load', function () {
   mapLoaded = true
   map.addSource('vehicles', { type: 'geojson', data: geojson })
   if (!initialFetchDone) fetchData()
@@ -36,8 +41,8 @@ map.on('load', function() {
     "type": "symbol",
     "source": "vehicles",
     "layout": {
-        "icon-image": "bus-15",
-        "icon-allow-overlap": true
+      "icon-image": "bus-15",
+      "icon-allow-overlap": true
     },
     "filter": ["==" , "type" , "bus"]
   })
@@ -46,83 +51,90 @@ map.on('load', function() {
     "type": "symbol",
     "source": "vehicles",
     "layout": {
-        "icon-image": "rail-15",
-        "icon-allow-overlap": true
+      "icon-image": "rail-15",
+      "icon-allow-overlap": true
     },
     "filter": ["==" , "type" , "rail"]
+  })
+
+  // Not many buses report this (still marked as experiemental)
+  // (Only for buses)
+  map.addLayer({
+    'id': 'inCongestion',
+    'type': 'circle',
+    'source': 'vehicles',
+    'paint': {
+      'circle-color': 'rgba(0,0,0,0)',
+      'circle-radius': 10,
+      'circle-stroke-width': 3,
+      'circle-stroke-color': '#f00'
+    },
+    'filter': ['==', 'inCongestion', true]
+  })
+
+  var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  })
+
+  map.on('mouseenter', 'bus', function (e) {
+    map.getCanvas().style.cursor = 'pointer'
+    popup.setLngLat(e.features[0].geometry.coordinates)
+      .setHTML(e.features[0].properties.sign)
+      .addTo(map)
+  })
+
+  map.on('mouseleave', 'bus', function () {
+    map.getCanvas().style.cursor = ''
+    popup.remove()
   })
 })
 
 function fetchData () {
-  console.log(archive)
+  if (fetchingData) return // todo: clean up stuff so we don't have to do this
+  var delayCount = 0
+  fetchingData = true
   archive.createReadStream('/vehicles.json')
     .pipe(ndjson.parse())
     .on('data', function (data) {
-      console.log(data)
+      // console.log(data)
+      if (data.properties.delay < 0) delayCount++
       geojson.features.push(data)
-      // geojson.features.push(point2GeoJSON(data))
       map.getSource('vehicles').setData(geojson)
     })
     .on('end', function () {
       console.log('initial fetch done')
       initialFetchDone = true
+      fetchingData = false
       archive.once('update', updateData)
+      console.log(delayCount)
     })
 }
 
-// function animate() {
-//   updateData(function () {
-//     console.log('animate')
-//     geojson.features = pendingData.pop()
-//     map.getSource('vehicles').setData(geojson)
-//     if (pendingData.length) return requestAnimationFrame(animate)
-//     archive.once('update', function () {
-//       requestAnimationFrame(animate)
-//     })
-//   })
-// }
-
 function updateData (cb) {
-  console.log('updating data')
   var features = []
   archive.createReadStream('/vehicles.json')
     .pipe(ndjson.parse())
     .on('data', function (data) {
       features.push(data)
-      // features.push(point2GeoJSON(data)) // TODO: update by ID & setData here
     })
     .on('end', function () {
       console.log('update done')
-
       geojson.features = features
       map.getSource('vehicles').setData(geojson)
       archive.once('update', updateData)
-
-      // pendingData.push(features)
-      // if (cb) return cb()
     })
 }
 
-function point2GeoJSON (data) {
-  return {
-    type: "Feature",
-    geometry: {
-      type: "Point",
-      coordinates: [data.longitude, data.latitude]
-    },
-    properties: data
-  }
-}
-
 function replicate () {
-  var wsProtocol =  (window.location.protocol === 'https:') ? 'wss://' : 'ws://'
+  var wsProtocol = (window.location.protocol === 'https:') ? 'wss://' : 'ws://'
   var ws = wss(wsProtocol + location.host) // ???
   ws.on('connect', function () {
     console.log('websockets connected')
+    // if we got disconnected during initial fetch, try again
     if (!initialFetchDone && mapLoaded) fetchData()
   })
   pump(ws, archive.replicate({live: true}), ws, function (err) {
-    // console.error('error', err)
-    setTimeout(replicate, 500) // again if it closes?
+    setTimeout(replicate, 500)
   })
 }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var trimetLive = require('trimet-live-archive')
 var storage = require('dat-storage')
 
 var app = express()
-var archive = hyperdrive('./archive') //storage('./data'), {latest: true})
+var archive = hyperdrive('./archive') // storage('./data'), {latest: true})
 var trimet = trimetLive(archive, {appID: process.env.APP_ID, geojson: true})
 var UPDATE_INTERVAL = 5000
 var port = process.env.PORT || 8080
@@ -21,20 +21,21 @@ archive.ready(function (err) {
   console.log('sharing archive on dat', archive.key.toString('hex'))
   discovery(archive)
 
-  // do an initial data fetch
-  trimet.fetch(function (err) {
-    if (err) throw err
-    console.log('fetched initial data')
+  if (archive.metadata.length > 1) return run()
 
+  // do an initial data fetch
+  trimet.fetch(run)
+
+  function run (err) {
+    if (err) throw err
     var server = app.listen(port, function () {
       console.log('Your app is listening on port ' + port)
     })
     wss.createServer({server: server}, onwebsocket)
 
-    // TODO: filling up glitch server space too fast, need this on a real server
     // Update data!
     setInterval(trimet.fetch, UPDATE_INTERVAL)
-  })
+  }
 })
 
 function onwebsocket (stream) {
